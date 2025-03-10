bafdfb87e3f6eff763113bfee9d226e682681b3d
osindu <x.osindu@gmail.com>
2025-03-10 23:42:24 +0530

    make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Profile from "./pages/Profile.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import EmailVerificationPage from './pages/EmailVerificationPage.jsx';
-
+import Dashboard from "./pages/Dashboard.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import EmailVerificationPage from "./pages/EmailVerificationPage.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route path="/verify-email" element={<EmailVerificationPage />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
-    <Footer />
+      <Footer />
     </BrowserRouter>
   );
 }
